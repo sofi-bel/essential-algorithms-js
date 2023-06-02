6ea181f7687e97680970c1545ea954ff7c1fef16
Sofi Bel <s_bel3@icloud.com>
2023-06-02 15:47:42 +0700

    improve search duplicate detection in array

--- 002-duplicate-detection.js
+++ 002-duplicate-detection.js
@@ -1,11 +1,11 @@
+"use strict"
+
 const numbers = [1, 2, 3, 4, 2]
 function containsDuplicates(numbers) {
-    for (let i = 0; i <= numbers.length; i += 1) {
-        for (let j = 0; j <= numbers.length; j += 1) {
-            if(i !== j) {
-                if(numbers[i] === numbers[j]) {
-                    return true;
-                }
+    for (let i = 0; i <= numbers.length - 1; i += 1) {
+        for (let j = i + 1; j <= numbers.length; j += 1) {
+            if(numbers[i] === numbers[j]) {
+                return true;
             }
         }
     }
